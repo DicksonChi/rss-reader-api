98eb5e2a366cf192ab2a5005f1defe95d891341c
Dickson <chibuzordick94@gmail.com>
2021-07-01 12:06:12 +0100

    .

--- rss_reader/rss_reader/settings.py
+++ rss_reader/rss_reader/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG') == 'True'
+DEBUG = False
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split('|')
 CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS', '').split('|')
